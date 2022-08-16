cf74dcc0078ddca94290d18fc7c848c58590c607
BK <borislav.kostov81@gmail.com>
2022-08-16 23:49:51 +0300

    Added the filter range between dates of orders. Added query params for filter date_start, date_end, metric

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.getData),
     path('add/', views.addProduct),
-    path('api/stats', views.orderStats),
+    path('api/stats', views.OrderAPIView.as_view()),
     path('order/', views.addOrder),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView, ListCreateAPIView
+from django_filters.rest_framework import DjangoFilterBackend
 from product.models import Product
 from order.models import Order
 from .serializers import ProductSerializer, OrderSerializer
@@ -26,11 +28,27 @@ def addOrder(request):
         serializer.save()
     return Response(serializer.data)
 
-@api_view(['GET'])
-def orderStats(request):
-    paginator = CustomPagination()
-    paginator.page_size = 3
-    orders = Order.objects.all()
-    paginated_orders = paginator.paginate_queryset(orders, request)
-    serilizer = OrderSerializer(paginated_orders, many=True)
-    return paginator.get_paginated_response(serilizer.data)
+class OrderAPIView(ListCreateAPIView):
+    serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend]
+    pagination_class = CustomPagination
+
+    filterset_fields = ['date']
+
+    def get_queryset(self):
+        filter = {}
+        queryset = Order.objects.all()
+
+        date_start = self.request.GET.get('date_start', None)
+        date_end = self.request.GET.get('date_end', None)
+        metric = self.request.GET.get('metric', None)
+
+        if date_start is not None:
+            filter['date__gte'] = date_start
+
+        if date_end is not None:
+            filter['date__lte'] = date_end
+
+        queryset = queryset.filter(**filter)
+
+        return queryset
