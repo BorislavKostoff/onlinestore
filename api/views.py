619d585debce3068bb75b0f1df275835d7a1c45c
BK <borislav.kostov81@gmail.com>
2022-08-18 23:42:08 +0300

    .

--- api/views.py
+++ api/views.py
@@ -43,6 +43,10 @@ class OrderAPIView(ListCreateAPIView):
         date_end = self.request.GET.get('date_end', None)
         metric = self.request.GET.get('metric', None)
 
+        result = {}
+        months = [0,0,0,0,0,0,0,0,0,0,0,0]
+        jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec = months
+
         if date_start is not None:
             filter['date__gte'] = date_start
 
@@ -50,5 +54,49 @@ class OrderAPIView(ListCreateAPIView):
             filter['date__lte'] = date_end
 
         queryset = queryset.filter(**filter)
+
+        # orders = queryset
+
+        # if metric == 'count':
+            #for order in orders['results']:
+            #    if "2022-01" in order['date']:
+            #        jan += len(order['products'])
+            #        result['2022 JAN'] = jan
+            #    if "2022-02" in order['date']:
+            #        feb += len(order['products'])
+            #        result['2022 FEB'] = feb
+            #    if "2022-03" in order['date']:
+            #        mar += len(order['products'])
+            #        result['2022 MAR'] = mar
+            #    if "2022-04" in order['date']:
+            #        apr += len(order['products'])
+            #        result['2022 APR'] = apr
+            #    if "2022-05" in order['date']:
+            #        may += len(order['products'])
+            #        result['2022 MAY'] = may
+            #    if "2022-06" in order['date']:
+            #        jun += len(order['products'])
+            #        result['2022 JUN'] = jun
+            #    if "2022-07" in order['date']:
+            #        jul += len(order['products'])
+            #        result['2022 JUL'] = jul
+            #    if "2022-08" in order['date']:
+            #        aug += len(order['products'])
+            #        result['2022 AUG'] = aug
+            #    if "2022-09" in order['date']:
+            #        sep += len(order['products'])
+            #        result['2022 SEP'] = sep
+            #    if "2022-10" in order['date']:
+            #        oct += len(order['products'])
+            #        result['2022 OCT'] = oct
+            #    if "2022-11" in order['date']:
+            #        nov += len(order['products'])
+            #        result['2022 NOV'] = nov
+            #    if "2022-12" in order['date']:
+            #        dec += len(order['products'])
+            #        result['2022 DEC'] = dec
+            #return result
+
+
 
         return queryset
